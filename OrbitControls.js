177043478267703d17ab407aba19e6c8971ed3a0
Ethan Kaplan <ethankaplan@Ethans-MacBook-Pro.local>
2019-11-03 19:58:32 -0800

    removed prevent default on touch

--- OrbitControls.js
+++ OrbitControls.js
@@ -965,7 +965,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		if ( scope.enabled === false ) return;
 
-		event.preventDefault();
+		//event.preventDefault();
 
 		switch ( event.touches.length ) {
 
